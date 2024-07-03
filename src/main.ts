521beafe4190750b6e0881034cea1e902f14ee30
uutbudiarto2012 <hikids.developer@gmail.com>
2024-07-03 16:01:35 +0700

    change cors

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['*'],
   });
   app.use(cookieParser());
-  await app.listen(4000);
+  await app.listen(8000);
 }
 bootstrap();
